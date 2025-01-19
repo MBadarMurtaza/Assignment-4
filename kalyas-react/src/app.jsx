95ab50392d2c53bec8365ac9335a438f3fb372d7
mBadar-dev <m.badar.dev@gmail.com>
2025-01-19 15:54:11 +0500

    run lower header portion

--- kalyas-react/src/app.jsx
+++ kalyas-react/src/app.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/header";
 import Footer from "./components/footer";
+import LowerHeader from "./components/lower header";
 
 const App = () => {
     return (
         <>
             <Header />
+            <LowerHeader />
             <Footer />
         </>
     );
