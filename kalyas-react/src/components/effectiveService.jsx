7851401ae6391f34f96c035d5692a0a23dc2dda2
mBadar-dev <m.badar.dev@gmail.com>
2025-01-19 17:36:33 +0500

    create a Services component

---
+++ kalyas-react/src/components/effectiveService.jsx
@@ -0,0 +1,64 @@
+const EffectiveService = () => {
+  return (
+    <>
+      <div className="w-[1400px] mx-auto bg-[#f8f8f8] h-[400px] py-[5rem]">
+        <div className="w-[1100px] mx-auto flex justify-between">
+          <div className="grid grid-cols-2 w-[48%] gap-14">
+            <div>
+              <h2 className="text-md font-bold mb-5">
+                01.Elegant / unique design
+              </h2>
+              <p className="text-[13px] text-[#8d8d8d]">
+                Malesuada fames ac turpis egestas maecenas pharetra convallis
+                posuere. Aliquam ut porttitor a sollicitudin.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-md font-bold mb-5">
+                02. True Responsiveness
+              </h2>
+              <p className="text-[13px] text-[#8d8d8d]">
+                At risus viverra adipiscing at in tellus. Nec tincidunt praesent
+                semper feugiat nibh. Nunc scelerisque viverra.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-md font-bold mb-5">
+                03. Different layout type
+              </h2>
+              <p className="text-[13px] text-[#8d8d8d]">
+                Sed arcu non odio euismod lacinia at quis risus. Turpis in eu mi
+                bibendum neque egestas congue quisque,
+              </p>
+            </div>
+            <div>
+              <h2 className="text-md font-bold mb-5">04. Make it style</h2>
+              <p className="text-[13px] text-[#8d8d8d]">
+                Imperdiet proin fermentum leo vel orci, Aenean pharetra magna ac
+                placerat vestibulum lectus mauris ultrices.
+              </p>
+            </div>
+          </div>
+          <div className="w-[48%] ">
+            <h1 className="text-4xl font-bold font-[#202020]">
+              We provide high quality and cost effective services.
+            </h1>
+            <p className="mt-11 text-sm font-light text-[#8d8d8d]">
+              Massa ultricies mi quis hendrerit dolor magna eget est lorem Amet
+              risus nullam eget felis eget. Orci dapibus ultrices in iaculis
+              nunc sed augue lacus viverra_ Blandit volutpat maecenas volutpat
+              blandit aliquam etiam erat velit scelerisque.
+            </p>
+            <p className="mt-10 text-sm font-light text-[#8d8d8d]">
+              Fringilla est ullamcorper eget nulla facilisi etiam dignissim
+              diam. Uma nunc id cursus metus aliquam eleifend. Imperdiet proin
+              fermentum leo vel orci.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EffectiveService;
