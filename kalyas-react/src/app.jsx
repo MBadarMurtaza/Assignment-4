fac8db31b3096fa65d2fda9a3daf88a0da0ac6fd
mBadar-dev <m.badar.dev@gmail.com>
2025-01-19 17:38:56 +0500

    import all the components in this file

--- kalyas-react/src/app.jsx
+++ kalyas-react/src/app.jsx
@@ -1,12 +1,28 @@
 import Header from "./components/header";
 import Footer from "./components/footer";
 import LowerHeader from "./components/lower header";
+import ServicesSection from "./components/services";
+import BusinessPlanSection from "./components/businessPlan";
+import EffectiveService from "./components/effectiveService";
+import Facilities from "./components/facilities";
+import Clients from "./components/clients";
+import Brand from "./components/brands";
+import Design from "./components/design";
+import Contact from "./components/contact";
 
 const App = () => {
     return (
         <>
             <Header />
             <LowerHeader />
+            <ServicesSection />
+            <BusinessPlanSection />
+            <EffectiveService />
+            <Facilities />
+            <Clients />
+            <Brand />
+            <Design />
+            <Contact />
             <Footer />
         </>
     );
